6b1cd59f779d1364a3d8dbe0b7200f00fb4a835b
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-07-23 00:30:34 +0900

    Initial commit

---
+++ src/test/java/com/miraclemorning/MiracleMorningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miraclemorning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiracleMorningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
